5ffa2f70fcd2c73785a324ac37cf18c0e4c09ac0
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2023-04-01 17:07:29 +0100

    Alteracoes fisicas da tabela

--- SoProd/SoProd.Web/Controllers/HomeController.cs
+++ SoProd/SoProd.Web/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using SoProd_Testing.Data;
 using SoProd_Testing.Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,26 +13,74 @@ namespace SoProd.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             using (var context = new SoProdTestingContext()) {
-                List<TestResultViewModel> list = new List<TestResultViewModel>();
-                var resultList = context.TestResults.ToList();
+                List<TestDefinitionViewModel> list = new List<TestDefinitionViewModel>();
 
-                foreach(var result in resultList)
+                var definitionsList = await context.TestDefinitions.ToListAsync();
+                var resultList = await context.TestResults.ToListAsync();
+
+                foreach(var result in definitionsList)
+                {
+                    TestDefinitionViewModel resultViewModel = new TestDefinitionViewModel();
+
+                    resultViewModel.Id = result.Id;
+                    resultViewModel.Name = result.Name;
+                    resultViewModel.UserCount = result.UserCount;
+                    resultViewModel.Version = result.Version;
+
+                    if (resultViewModel.Version == null)
+                        resultViewModel.Version = "N/A";
+
+                    resultViewModel.BaseAddress = result.BaseAddress;
+                    resultViewModel.testResults = new List<TestResultViewModel>();
+
+                    list.Add(resultViewModel);
+                }
+
+                for (int i = 0; i < 15; i++)
                 {
                     TestResultViewModel resultViewModel = new TestResultViewModel();
 
-                    resultViewModel.Id = result.TestDefinitionId;
-                    resultViewModel.Identifier = result.Identifier;
-                    resultViewModel.TimeEllapsed = result.TimeEllapsed;
+                    resultViewModel.Id = resultList[i].TestDefinitionId;
+                    resultViewModel.Identifier = resultList[i].Identifier;
+                    resultViewModel.TimeEllapsed = resultList[i].TimeEllapsed;
+                    resultViewModel.StartDate = resultList[i].StartDate;
+                    resultViewModel.Version = resultList[i].Version;
+                    resultViewModel.RequestsNumber = resultList[i].RequestsNumber;
+                    resultViewModel.RequestsOK = resultList[i].RequestsOK;
+                    resultViewModel.RequestsError = resultList[i].RequestsError;
 
-                    list.Add(resultViewModel);
+                    foreach(var definition in list)
+                    {
+                        if(definition.Id == resultList[i].TestDefinitionId)
+                            definition.testResults.Add(resultViewModel);
+                    }
                 }
 
                 return View(list);
             }
         }
+
+        //public async Task<JsonResult> GetThemes(int flowId = -1)
+        //{
+        //    using (var context = new SoProdTestingContext())
+        //    {
+        //        List<string[]> list = new List<string[]>();
+        //        var data = await context.TestResults.Distinct.ToListAsync();
+
+        //        foreach (var item in data)
+        //        {
+        //            list.Add(new string[] { Languages.Get("All"), item.Version, item.Version });
+        //        }
+
+        //        return Json(new
+        //        {
+        //            data = list
+        //        });
+        //    }
+        //}
 
         public ActionResult About()
         {
--- SoProd/SoProd.Web/Models/TestResultViewModel.cs
+++ SoProd/SoProd.Web/Models/TestResultViewModel.cs
@@ -5,6 +5,17 @@ using System.Web;
 
 namespace SoProd.Web.Models
 {
+    public class TestDefinitionViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserCount { get; set; }
+        public string Version { get; set; }
+        public string BaseAddress { get; set; }
+        //public int MaxErrorCodes { get; set; }
+        public List<TestResultViewModel> testResults { get; set; }
+    }
+
     public class TestResultViewModel
     {
         public int Id { get; set; }
@@ -15,7 +26,6 @@ namespace SoProd.Web.Models
         public int RequestsNumber { get; set; }
         public int RequestsOK { get; set; }
         public int RequestsError { get; set; }
-        public string Scenarios { get; set; }
 
     }
 }
